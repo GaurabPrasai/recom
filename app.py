4941b762fcad0506ef479259e82f1f4eeb46fdf7
Bipin Thapa <142653093+Tailung42@users.noreply.github.com>
2024-07-09 21:59:04 +0545

    remove debugging print statements
    it's not a good practice to keep print statements after the application has been hosted

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import requests
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=420b8821330cec3f214163c75423281c&language=en-US'.format(movie_id))
     data = response.json()
-    print(data)
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 # Load movies data
