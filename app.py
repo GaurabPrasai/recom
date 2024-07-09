6a6761b18156bf80e4c62069b51c4fcc9560b498
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-07-09 06:05:21 +0000

    idk wth is this

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import requests
+import os
 
-def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=420b8821330cec3f214163c75423281c&language=en-US'.format(movie_id))
-    data = response.json()
-    print(data)
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+st.title('Movie Recommender System')
+
+def load_data(filename):
+    if not os.path.exists(filename):
+        st.error(f"Error: The file '{filename}' does not exist.")
+        return None
+    
+    try:
+        with open(filename, 'rb') as file:
+            data = pickle.load(file)
+        return data
+    except Exception as e:
+        st.error(f"Error loading {filename}: {str(e)}")
+        return None
 
 # Load movies data
-movies_list = pickle.load(open('movies.pkl', 'rb'))
+movies_list = load_data('movies.pkl')
+if movies_list is None:
+    st.stop()
+
 if isinstance(movies_list, pd.DataFrame):
     movies = movies_list
 else:
@@ -22,43 +34,29 @@ if 'title' not in movies.columns:
     st.stop()
 
 # Load similarity matrix
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+similarity = load_data('similarity.pkl')
+if similarity is None:
+    st.stop()
+
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
-    recommended_movies_posters = []
     for i in movie_list:
-        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        # Fetch poster from API
-        recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies,recommended_movies_posters
-
-st.title('Movie Recommender System')
+    return recommended_movies
 
 selected_movie_name = st.selectbox(
     "Select a movie", movies['title'].tolist())
 
-
 if st.button("Get Recommendations"):
-    names,posters = recommend(selected_movie_name)
+    try:
+        recommendations = recommend(selected_movie_name)
+        for i in recommendations:
+            st.write(i)
+    except Exception as e:
+        st.error(f"An error occurred while getting recommendations: {str(e)}")
 
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
+st.info("If you're experiencing issues, please check that your 'movies.pkl' and 'similarity.pkl' files are up to date and compatible with your current Python version.")
